dc496ce7c4bacdad89bf4f1ef63081c9aaada0cf
Prakash <cjprakash522@gmail.com>
2024-04-25 17:01:28 +0530

    first commit

---
+++ src/test/java/com/sathya/security/SpringSecurityEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathya.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
